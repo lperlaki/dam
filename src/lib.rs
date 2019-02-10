ff63f985b9a53cbe2fbda01af7e19a9743570509
Liam Perlaki <lperlaki@icloud.com>
2019-02-10 20:31:31 +0100

    sore in leveldb

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,59 @@
 use checksum::crc::Crc;
+use leveldb::database::error::Error;
+use leveldb::database::Database;
+use leveldb::kv::KV;
+use leveldb::options::{Options, WriteOptions};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
-struct Info {
-    name: std::ffi::OsString,
-    checksum: u64,
-    filetype: fs::FileType,
-    meta: fs::Metadata,
-    path: Box<Path>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Info {
+    name: String,
+    checksum: i32,
+    path: PathBuf,
 }
 
 impl Info {
     fn from_entry(entry: fs::DirEntry) -> io::Result<Info> {
         let path = entry.path();
         Ok(Info {
-            name: entry.file_name(),
+            name: match entry.file_name().into_string() {
+                Ok(s) => s,
+                Err(_) => String::new(),
+            },
             checksum: path.checksum()?,
-            filetype: entry.file_type()?,
-            meta: path.metadata()?,
-            path: path.into_boxed_path(),
+            path: path,
         })
+    }
+    fn save(&self, db: &Database<i32>) -> Result<(), Error> {
+        let write_opts = WriteOptions::new();
+        db.put(
+            write_opts,
+            &self.checksum,
+            serde_json::to_vec(&self).unwrap().as_ref(),
+        )
+    }
+    pub fn from_bytes(bytes: &[u8]) -> io::Result<Info> {
+        match serde_json::from_slice(bytes) {
+            Ok(val) => Ok(val),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
+        }
     }
 }
 
 trait Information {
-    fn checksum(&self) -> io::Result<u64>;
+    fn checksum(&self) -> io::Result<i32>;
     fn metadata(&self) -> io::Result<fs::Metadata>;
 }
 
 impl Information for Path {
-    fn checksum(&self) -> io::Result<u64> {
+    fn checksum(&self) -> io::Result<i32> {
         if let Some(filename) = self.to_str() {
             let mut crc = Crc::new(filename);
             return match crc.checksum() {
-                Ok(checksum) => Ok(checksum.crc64),
+                Ok(checksum) => Ok(checksum.crc32 as i32),
                 Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
             };
         }
@@ -50,7 +68,18 @@ pub fn create_path(p: &Path) -> std::io::Result<()> {
     fs::create_dir_all(&p)
 }
 
-pub fn visit_dirs(dir: &Path) -> io::Result<()> {
+pub fn create_dir_db(dir: &Path) -> Database<i32> {
+    let dir = dir.join(".db");
+    let path = dir.as_path();
+    let mut options = Options::new();
+    options.create_if_missing = true;
+    match Database::<i32>::open(path, options) {
+        Ok(db) => db,
+        Err(e) => panic!("failed to open database: {:?}", e),
+    }
+}
+
+pub fn visit_dirs(dir: &Path, db: &Database<i32>) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)?.filter(|s| match s {
             Ok(s) => !s.file_name().into_string().unwrap().starts_with("."),
@@ -59,10 +88,14 @@ pub fn visit_dirs(dir: &Path) -> io::Result<()> {
             let entry = entry?;
             let path = entry.path();
             if path.is_file() {
-                println!("{:?}", Info::from_entry(entry)?);
+                let info = Info::from_entry(entry)?;
+                match info.save(&db) {
+                    Ok(_) => (),
+                    Err(e) => panic!("failed to write to database: {:?}", e),
+                };
             }
             if path.is_dir() {
-                visit_dirs(&path)?;
+                visit_dirs(&path, &db)?;
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![feature(box_syntax, uniform_paths)]
-#![allow(unused_variables, dead_code)]
+#![allow(unused_variables, dead_code, unused_imports)]
 
 mod app;
 mod lib;
+use leveldb::database::Database;
+use leveldb::iterator::Iterable;
+use leveldb::kv::KV;
+use leveldb::options::{Options, ReadOptions, WriteOptions};
 use lib::*;
 use std::path::Path;
 
@@ -10,5 +14,14 @@ fn main() -> std::io::Result<()> {
     // let matches = app::build().get_matches();
     // create_path(Path::new("./tmp/root/1/2"))
     // fs::rename("./test", "./root/test")
-    visit_dirs(Path::new("./tmp"))
+    let path = Path::new("./tmp");
+    let database = create_dir_db(&path);
+    visit_dirs(&path, &database)?;
+    let read_opts = ReadOptions::new();
+    let iter = database.iter(read_opts);
+    let entrys = iter
+        .map(|val| Info::from_bytes(val.1.as_ref()).unwrap())
+        .collect::<Vec<Info>>();
+    println!("{:?}", entrys);
+    Ok(())
 }
