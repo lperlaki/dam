deab95b499df5bcc8c997521aaffa0f8c0f817a4
Liam Perlaki <lperlaki@icloud.com>
2019-02-11 15:21:06 +0100

    basic cli

--- src/app.rs
+++ src/app.rs
@@ -1,39 +1,24 @@
 use clap::{App, Arg, SubCommand};
+use std::path::PathBuf;
 
 pub fn build() -> App<'static, 'static> {
     App::new("Open DAM")
-        .version("1.0")
+        .version("0.1.0")
         .author("Liam P. <lperlaki@icloud.com>")
-        .about("Didital Asset Manager")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .takes_value(true),
-        )
+        .about("Digital Asset Manager")
         .arg(
             Arg::with_name("INPUT")
                 .help("Sets the input file to use")
                 .required(true)
+                .default_value(".")
+                .validator(|val| {
+                    let path = PathBuf::from(val);
+                    if path.exists() && path.is_dir() {
+                        Ok(())
+                    } else {
+                        Err(String::from("Must be a valid directory!"))
+                    }
+                })
                 .index(1),
-        )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .subcommand(
-            SubCommand::with_name("test")
-                .about("controls testing features")
-                .version("1.3")
-                .author("Someone E. <someone_else@other.com>")
-                .arg(
-                    Arg::with_name("debug")
-                        .short("d")
-                        .help("print debug information verbosely"),
-                ),
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,12 @@ use dam::*;
 
 use std::path::Path;
 
+
+
 fn main() -> Result<()> {
-    // let matches = app::build().get_matches();
-    // create_path(Path::new("./tmp/root/1/2"))
-    let path = Path::new("./tmp");
+    let matches = app::build().get_matches();
+    let path = Path::new(matches.value_of("INPUT")?);
     let database = create_dir_db(path)?;
-    // visit_dirs(&path, &database)?;
-    // database.read(|db| println!("{:?}", db.get(&0)))?;
-    database.write(|db| {
-        db.entry(0).and_modify(|entry| {
-            entry.rename(Path::new("./tmp/root/another"));
-        });
-    })?;
-    database.save()?;
+    visit_dirs(&path, &database)?;
     Ok(())
 }
